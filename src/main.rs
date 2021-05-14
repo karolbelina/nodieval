4c8e64040515e57b48e947ffb1ee21db988c2d60
Karol Belina <karolbelina@gmail.com>
2021-05-15 00:40:11 +0200

    Modify the release workflow to upload the artifacts

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,19 @@
 #[cfg(target_arch = "wasm32")]
 use bevy_webgl2;
 
-use bevy::prelude::{App, ClearColor, Color, WindowDescriptor};
+use bevy::prelude::{App, ClearColor, Color, WindowDescriptor, Msaa};
 use bevy::DefaultPlugins;
 use game_plugin::GamePlugin;
 
 fn main() {
     let mut app = App::build();
     app
-        // .insert_resource(Msaa { samples: 4 })
+        .insert_resource(Msaa { samples: 4 })
         .insert_resource(ClearColor(Color::rgb(0.4, 0.4, 0.4)))
         .insert_resource(WindowDescriptor {
             width: 800.,
             height: 600.,
-            title: "Nodieval".to_string(), // ToDo
+            title: "Nodieval".to_string(),
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
