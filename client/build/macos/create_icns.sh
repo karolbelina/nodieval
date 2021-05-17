89ce2bab8e9303ef0735469847a6dc5b095b1257
Karol Belina <karolbelina@gmail.com>
2021-05-17 23:22:05 +0200

    Introduce the icon creation script for the macOS build workflow

--- client/build/macos/create_icns.sh
+++ client/build/macos/create_icns.sh
@@ -1,4 +1,3 @@
-rm -rf AppIcon.iconset/*
 mkdir -p AppIcon.iconset
 sips -z 16 16     icon_1024x1024.png --out AppIcon.iconset/icon_16x16.png
 sips -z 32 32     icon_1024x1024.png --out AppIcon.iconset/icon_16x16@2x.png
